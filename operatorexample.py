e1a6453ecbfaaecf4f2354188ffc7fbff8455531
Mikias <mikimatrakey@gmail.com>
2024-06-19 16:54:36 +0300

    Operator

--- operatorexample.py
+++ operatorexample.py
@@ -1,5 +1,12 @@
-# Arthmetic Operator
-num1=12
+"""num1=45
 num2=23
 num3=num1+num2
-print(num3)
+num3+=10
+print(num3>=num1)"""
+
+# logical and or not
+num1=25
+print(num1>10 or num1<20)
+print(num1>10 and num1<20)
+name="Yoseph"
+print("M" not in name)

94b253672d28fc1e9f99936a461a7e6595ad391a
Mikias <mikimatrakey@gmail.com>
2024-06-19 17:08:10 +0300

    Loop

---
+++ conditional.py
@@ -0,0 +1,17 @@
+#>90, "A",>80,"B",>70,"C"
+mark=int(input("Please enter Grade: "))
+if mark>90:
+    grade="A"
+elif mark>80:
+    grade="B"
+elif mark>70:
+    grade="C"
+elif mark>60:
+    grade="D"
+elif mark>50:
+    grade="E"
+else:
+    grade="F"    
+print(grade)    
+    
+            
